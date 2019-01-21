e3538522c6079b878a9893a4256328ff80a4e5ea
Александр Горбунов <A.Gorbunov@webcom.lan>
2019-01-21 15:17:57 +0300

    'первый_коммит_в_мастер_ветке'

--- index.php
+++ index.php
@@ -1,8 +1,4 @@
 <?php
 
-echo 'Hello, world!';
-echo 'add: git branch testing,  no ckechout';
-echo 'git: перешел в ветку master';
-echo 'git: git checkout Testing, итого: файл использует своё состояние на момент создания Testing';
-echo 'новый текст в ветке testing';
+echo 'new commit!';
 

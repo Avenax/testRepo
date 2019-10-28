e2fbe890c6ae281dcc9bab0e25e197f201d88e69
Александр <pro_ak48@mail.ru>
2019-10-28 21:32:00 +0300

    Update index.php

--- index.php
+++ index.php
@@ -11,6 +11,35 @@ function pathRoot() {
 	return (CMain::IsHTTPS() ? "https://" : "http://") . $_SERVER['SERVER_NAME'];
 }
 
+function time_elapsed_string($datetime, $full = false) {
+    $now = new DateTime;
+    $ago = new DateTime($datetime);
+    $diff = $now->diff($ago);
+
+    $diff->w = floor($diff->d / 7);
+    $diff->d -= $diff->w * 7;
+
+    $string = array(
+        'y' => 'year',
+        'm' => 'month',
+        'w' => 'week',
+        'd' => 'day',
+        'h' => 'hour',
+        'i' => 'minute',
+        's' => 'second',
+    );
+    foreach ($string as $k => &$v) {
+        if ($diff->$k) {
+            $v = $diff->$k . ' ' . $v . ($diff->$k > 1 ? 's' : '');
+        } else {
+            unset($string[$k]);
+        }
+    }
+
+    if (!$full) $string = array_slice($string, 0, 1);
+    return $string ? implode(', ', $string) . ' ago' : 'just now';
+}
+
 function getTimeIso8601($TIMESTAMP_X) {
 	$datetime = new DateTime($TIMESTAMP_X);
 	return $datetime->format('c');
